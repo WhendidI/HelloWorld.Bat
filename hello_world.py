532f687c9f7ebe31f6748b3ba44dc7b4c6352c7b
User <142289541+WhendidI@users.noreply.github.com>
2023-08-15 18:08:39 +1200

    Update and rename hello_world.bat to hello_world.py
    take 2, Git Hub can't run .bat, 
    so I'm going back to python. 
    
    this shall be my platform to practice python projects and display them here. 
    
    also wana learn other stuff to. 
    I think if I make an effort to code at least 1 project a day I can make some huge improvements

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# ok scratch that, changing it up to .py... coz GitHub can't run .bat.
+# wonder if it can even run pyhton imports 
+#
+# meh
+#¯\_(ツ)_/¯
+
+#we can cross that bridge when we get there. 
+#shit might not even cross it at all
+#might just use GitHub as storage for old projects
+
+print ('hello world!!!')
